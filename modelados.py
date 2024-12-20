84d206bba02608f21318fdcb79007b9beaae0d68
beto <estadistico1984gmail.com>
2024-12-20 19:22:20 -0300

    agregamos la distancia de riesgo calculandola

--- modelados.py
+++ modelados.py
@@ -12,9 +12,11 @@ def radar_atomico(distancia_misil:float,
     
     """ Esta funcion es un radar para detectar misiles
     dado un cierto umbral de distancia a mi base de guerra"""
+    
     factor_aleatorio = np.abs(np.cos(angulo) + 0.98*np.exp(-angulo))
     
     umbral = np.sqrt((localizacion[0] - localizacion[1])**2 )
+    
     
     print("factor umbral permitido", umbral)
     
@@ -22,10 +24,13 @@ def radar_atomico(distancia_misil:float,
         
         distancia_misil = np.sqrt(distancia_misil + factor_aleatorio)
         
+        distancia_riesgo = (distancia_misil - ( 5 + 10*umbral))
+        
         if distancia_misil < 5 + 10*umbral:
             
             print("misil esta cerca de la base, evacuar")
             print("valor umbral permitido", 5 + 10*umbral)
+            print("distancia de riesgo", distancia_riesgo)
             
         else:
             print("misil no esta cerca de la base, estar atentos")
@@ -43,6 +48,8 @@ if __name__ == "__main__":
     angulo = 0.98
     localizacion = (12.3,11.5)
     detectados = True
-    print(radar_atomico(distancia,umbral,angulo=angulo, localizacion = localizacion ,misiles_detectados = detectados))
+    print(radar_atomico(distancia,umbral,angulo=angulo, 
+                        localizacion = localizacion ,
+                        misiles_detectados = detectados))
 
     
