75886566531ae2e0f8bb71708dda8503b4af196a
beto <estadistico1984gmail.com>
2024-12-19 22:28:42 -0300

    aca agregamos mas criterios numericos al humbral y calculamos coordenadas de posicion

--- modelados.py
+++ modelados.py
@@ -11,15 +11,20 @@ def radar_atomico(distancia_misil:float,
                   misiles_detectados = False):
     
     """ Esta funcion es un radar para detectar misiles"""
-    factor_aleatorio = np.cos(angulo) + np.exp(-angulo)
+    factor_aleatorio = np.abs(np.cos(angulo) + np.exp(-angulo))
+    
+    umbral = np.sqrt((localizacion[0] - localizacion[1])**2 )
+    
+    print("factor umbral permitido", umbral)
     
     if misiles_detectados:
         
         distancia_misil = np.sqrt(distancia_misil + factor_aleatorio)
         
-        if distancia_misil < umbral:
+        if distancia_misil < 5 + 10*umbral:
             
             print("misil esta cerca de la base, evacuar")
+            print("valor umbral permitido", 5 + 10*umbral)
             
         else:
             print("misil no esta cerca de la base, estar atentos")
@@ -31,13 +36,12 @@ def radar_atomico(distancia_misil:float,
     return print("distancia misil", distancia_misil)
         
 
-        
-
 if __name__ == "__main__":
     distancia = 10
     umbral  = 12
     angulo = 0.98
+    localizacion = (12.3,11.5)
     detectados = True
-    print(radar_atomico(distancia,umbral,angulo=angulo ,misiles_detectados = detectados))
-    
+    print(radar_atomico(distancia,umbral,angulo=angulo, localizacion = localizacion ,misiles_detectados = detectados))
+
     
